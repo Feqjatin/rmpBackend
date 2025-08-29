6705a3470ce58180f5f96e82181827109b2a2cc6
JAtin-know <prajapatijatinm1@gmail.com>
2025-08-29 16:31:25 +0530

    first setup + test crud

---
+++ rmpBackend/Controllers/FirstController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using rmpBackend.Models;
+
+namespace rmpBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> getDummy()
+        {
+            List<string> s1 = new List<string>() { "jatin", "jatin1", "jatin2" };
+            return s1;
+        }
+        [HttpGet("{2}")]
+        public List<string> getDummy2()
+        {
+            List<string> s1 = new List<string>() { "use2" };
+            return s1;
+        }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class dbController : ControllerBase
+    {
+        private readonly jatinYYContext db;
+
+        public dbController(jatinYYContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Person>>> read()
+        {
+            var data = await db.People.ToListAsync();
+            return Ok(data);
+        }
+    }
+}
---
+++ rmpBackend/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace rmpBackend.Models;
+
+[Keyless]
+public partial class Person
+{
+    public int? Id { get; set; }
+
+    [StringLength(10)]
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
+}
